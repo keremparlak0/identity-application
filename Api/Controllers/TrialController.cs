ed50c9f29aa3abc7a65d58914a46609927a90454
kparlak <kparlak@borcelik.com>
2024-03-05 18:56:46 +0300

    Identity project was created

---
+++ Api/Controllers/TrialController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrialController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAraBulGetir()
+        {
+            return Ok("Get hele");
+        }
+    }
+}
