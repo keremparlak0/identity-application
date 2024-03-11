4491042944c528193603118181541095df6b3729
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2024-03-11 23:52:01 +0300

    register-user

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Api.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -24,15 +25,6 @@ namespace Api.Controllers
             _userManager = userManager;
         }
 
-        private async Task<UserDto> CreateAplicationUserDtoAsync(User user)
-        {
-            return new UserDto
-            {
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                JWT = await _jwtService.CreateJWT(user)
-            };
-        }
 
         [HttpPost]
         public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto model)
@@ -52,6 +44,46 @@ namespace Api.Controllers
                 return Unauthorized("Invaild username or password");
 
             return await CreateAplicationUserDtoAsync(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> Register([FromBody] RegisterDto model)
+        {
+            if (await CheckEmailExistsAsync(model.Email))
+            {
+                return BadRequest($"An existing account is using {model.Email}, email address. Please try with another email address.");
+            }
+
+            var userToAdd = new User
+            {
+                FirstName = model.FirstName.ToLower(),
+                LastName = model.LastName.ToLower(),
+                Email = model.Email.ToLower(),
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(userToAdd, model.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return Ok("Your account has been created.");
+        }
+
+
+
+
+        private async Task<UserDto> CreateAplicationUserDtoAsync(User user)
+        {
+            return new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                JWT = await _jwtService.CreateJWT(user)
+            };
+        }
+
+        private async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _userManager.Users.AnyAsync(x => x.Email == email.ToLower());
         }
     }
 }
